a1bb14c2923d6a0d3159b5954819589281ff3b44
Robert fletcher <iknowfletchstuff@yahoo.com>
2019-02-18 21:00:03 -0500

    Called the Add function

--- Project1HelloWorld/com/project1/HelloWorld.py
+++ Project1HelloWorld/com/project1/HelloWorld.py
@@ -6,8 +6,9 @@ Created on Feb 18, 2019
 import com.project1.Functions
 
 print("Hello World")
-
 com.project1.Functions.func()
+print ("calling add") 
+print ( com.project1.Functions.add(3,4) )
 
 if __name__ == '__main__':
     pass
