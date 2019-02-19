626c12628c6a45d9af428a939898ac9b01decfc4
Robert fletcher <iknowfletchstuff@yahoo.com>
2019-02-18 20:44:29 -0500

    Initial Commit

---
+++ Project1HelloWorld/com/project1/Functions.py
@@ -0,0 +1,20 @@
+'''
+Created on Feb 18, 2019
+
+@author: fleth
+'''
+
+def func() :
+    print("HelloWorld2 - func")
+    
+def add ( a,b ):
+    return a+b
+
+
+def sub( a,b ):
+    return a-b
+    
+
+
+if __name__ == '__main__':
+    pass
---
+++ Project1HelloWorld/com/project1/HelloWorld.py
@@ -0,0 +1,13 @@
+'''
+Created on Feb 18, 2019
+
+@author: fleth
+'''
+import com.project1.Functions
+
+print("Hello World")
+
+com.project1.Functions.func()
+
+if __name__ == '__main__':
+    pass
